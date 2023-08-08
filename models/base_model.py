4fb52c619e9ad2816c9fd052cfb4c94d3ac765a4
Younis ahmed <younisalx-swe@hotmail.com>
2023-08-08 14:43:09 +0300

    __str__ method with correct format

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,21 @@ class BaseModel:
     
     """BaseModel that defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
+        """
+            Public instances attributes:
+            id: unique identifier for instances in string data type
+            created_at: datetime of creation
+            updated_at: datetime of update
+        """
         self.id = str(uuid.uuid4())
         self.created_at = dt.now()
         self.updated_at = dt.now()
+
+        def __str__(self):
+            """
+                String representation of class
+            """
+            return f"[<{self.__class__.name}>] (<{self.id}>) <{self.__dict__}>"
+        
+
 
