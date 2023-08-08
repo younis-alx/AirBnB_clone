3535a510b1bd2bf8d2e05ab4a5a25f507d4ffd0e
Younis ahmed <younisalx-swe@hotmail.com>
2023-08-08 15:13:05 +0300

    to_dict method for dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -22,13 +22,26 @@ class BaseModel:
             """
                 String representation of class
             """
-            return f"[<{self.__class__.name}>] (<{self.id}>) <{self.__dict__}>"
+            return (f"[<{self.__class__.name}>] (<{self.id}>) <{self.__dict__}>")
         
         def save(self):
             """
                 updates the public instance attribute updated_at with the current datetime
             """
             self.updated_at = dt.now()
+
+        def to_dict(self):
+            """
+               returns a dictionary containing all keys/values of __dict__ of the instance 
+            """
+
+            CpyObj = self.__dict__.copy() # a copy of __dict__
+            CpyObj['__class__'] = self.__class__.__name__
+            CpyObj['created_at'] = self.created_at.isoformat()
+            CpyObj['updated_at'] = self.updated_at.isoformat()
+            return (CpyObj)
+
+        
 
 
 
