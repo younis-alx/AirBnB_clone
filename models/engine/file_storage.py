814ac8caed7c7fac2fa43a880ba3366577ace879
Younis ahmed <younisalx-swe@hotmail.com>
2023-08-09 17:11:25 +0300

    filestorge, engine initialization

---
+++ models/engine/file_storage.py
@@ -0,0 +1,15 @@
+import json
+
+class FileStorage:
+    """
+        class that serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+
+
