06ed1eca33c65467924184b12101b64d2c491e4a
Younis ahmed <younisalx-swe@hotmail.com>
2023-08-10 09:56:54 +0300

    save method serialize object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,7 @@ class FileStorage:
     """
         class that serializes instances to a JSON file and deserializes JSON file to instances
     """
-    __file_path = "file.json"
+    __file_path = ""
     __objects = {}
 
     def all(self):
@@ -14,7 +14,11 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         obj_key = str(obj.___class___.___name___) + '.' + str(obj.id)
-        
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, mode='w', encoding="UTF8") as json_file:
+           json.dump(FileStorage.__objects,json_file)
 
 
 
