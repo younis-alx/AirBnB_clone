fe9fcf51d6bec4c9835c1fe0a100f1253e8a60c6
Younis ahmed <younisalx-swe@hotmail.com>
2023-08-10 13:22:40 +0300

    cmd implementation quit, EOF handled

--- console.py
+++ console.py
@@ -1,4 +1,24 @@
+#!/usr/bin/python3
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """quite the program"""
+
+        return True
+    
+    def do_EOF(self, line):
+        """Exit the program"""
+        print()
+        return True
+    
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
+if __name__ == '__main__':
+        HBNBCommand().cmdloop()
+
