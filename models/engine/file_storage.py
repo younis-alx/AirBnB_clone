d662e4c06754ec3bbc8406bd12865c1ff747b5b8
Younis ahmed <younisalx-swe@hotmail.com>
2023-08-10 11:24:55 +0300

    reload bug fixes and serialize/deserialization cycle completed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,7 @@ class FileStorage:
     """
         class that serializes instances to a JSON file and deserializes JSON file to instances
     """
-    __file_path = ""
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -13,17 +13,29 @@ class FileStorage:
     
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        obj_key = str(obj.___class___.___name___) + '.' + str(obj.id)
+        obj_key = str(obj.__class__.__name__) + '.' + str(obj.id)
+        val_dict = obj
+        FileStorage.__objects[obj_key] = val_dict
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        
+        obj_dict = {}
+        
+        for key, val in FileStorage.__objects.items():
+            obj_dict[key] = val.to_dict()
+        
         with open(FileStorage.__file_path, mode='w', encoding="UTF8") as json_file:
-           json.dump(FileStorage.__objects,json_file)
+           json.dump(obj_dict,json_file)
 
     def reload(self):
-        """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        """Deserializes the JSON file to __objects if the file exists; otherwise, do nothing."""
+        from models.base_model import BaseModel
+
         try:
-            with open(FileStorage.__file_path, mode='r', encoding="UTF8") as obj:
-                FileStorage.__objects = json.load(obj)
+            with open(FileStorage.__file_path, encoding="UTF8") as json_file:
+                obj_dict = json.load(json_file)
+            for key, val in obj_dict.items():
+                FileStorage.__objects[key] = BaseModel(**val)
         except FileNotFoundError:
             pass
