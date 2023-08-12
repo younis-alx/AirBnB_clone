ce7e8250df7b572f5af77974754f40770694bed9
Younis ahmed <younisalx-swe@hotmail.com>
2023-08-12 13:15:23 +0300

    destroy method handled

--- console.py
+++ console.py
@@ -87,7 +87,8 @@ class HBNBCommand(cmd.Cmd):
         except KeyError:
             print("** no instance found **")
         storage.save()
-        
+
+    
          
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
