57c713d103495cd9782777bb8d91f35794586d59
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-08-12 22:23:43 -0700

    pep8 formatting __init__

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@
     create a unique FileStorage instance for your application
 """
 
-from models.engine.file_storage import FileStorage 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
