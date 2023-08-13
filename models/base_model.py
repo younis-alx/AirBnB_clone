f266f93c09516944c4acb70704dd283ffe8ac142
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-08-12 22:22:58 -0700

    pep8 formatting BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,13 @@ import uuid
 from datetime import datetime as dt
 from models import storage
 
+
 class BaseModel:
-    
-    """BaseModel that defines all common attributes/methods for other classes"""
+
+    """
+    BaseModel that defines all common\
+    attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
         """
             Public instances attributes:
@@ -16,13 +20,19 @@ class BaseModel:
         """
         if kwargs:
 
-            kwargs["created_at"] = dt.strptime(kwargs["created_at"],  "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = dt.strptime(kwargs["updated_at"],  "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["created_at"] = dt.strptime(
+                                               kwargs["created_at"],
+                                               "%Y-%m-%dT%H:%M:%S.%f"
+                                   )
+            kwargs["updated_at"] = dt.strptime(
+                                               kwargs["updated_at"],
+                                               "%Y-%m-%dT%H:%M:%S.%f"
+                                   )
 
             for key, val in kwargs.items():
                 if "__class__" not in key:
                     setattr(self, key, val)
-        else:        
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = dt.now()
             self.updated_at = dt.now()
@@ -33,27 +43,23 @@ class BaseModel:
             String representation of class
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-        
+
     def save(self):
         """
-            updates the public instance attribute updated_at with the current datetime
+            updates the public instance\
+            attribute updated_at with the current datetime
         """
         self.updated_at = dt.now()
         storage.save()
 
-
     def to_dict(self):
         """
-            returns a dictionary containing all keys/values of __dict__ of the instance 
+            returns a dictionary containing all\
+            keys/values of __dict__ of the instance
         """
 
-        cpy_obj = self.__dict__.copy() # a copy of __dict__
+        cpy_obj = self.__dict__.copy()  # a copy of __dict__
         cpy_obj['__class__'] = self.__class__.__name__
         cpy_obj['created_at'] = self.created_at.isoformat()
         cpy_obj['updated_at'] = self.updated_at.isoformat()
         return cpy_obj
-
-        
-
-
-
